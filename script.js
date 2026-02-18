aa0388023028aeeaff1263a8c43d48c9bf0f37b3
atonu-a <166700173+atonu-a@users.noreply.github.com>
2026-02-18 20:35:53 +0600

    New projects and styles added

--- script.js
+++ script.js
@@ -8,28 +8,11 @@ visit.addEventListener("click" , () =>{
   window.location.href = "#project-section-container";
 });
 
+
 //cv download
 let cvBtn = document.querySelector("#button");
 cvBtn.addEventListener("click", () =>{
   window.location.href="https://drive.google.com/file/d/15NHHyEb-An9u1KfJyjtGBjn95h-NOXgW/view?usp=drive_link";
-});
-
-// project 1 button
-let project1 = document.querySelector("#project1btn");
-project1.addEventListener("click", () =>{
-  window.location.href = "https://travelo-49wj.onrender.com";
-});
-
-// project 1 button
-let project2 = document.querySelector("#project2btn");
-project2.addEventListener("click", () =>{
-  window.location.href = "https://career-io.onrender.com";
-});
-
-// project 1 button
-let project3 = document.querySelector("#project3btn");
-project3.addEventListener("click", () =>{
-  window.location.href = "https://goodspanda.vercel.app/";
 });
 
 
@@ -45,3 +28,59 @@ const cursor = Cursorly.init({
   cursor: 21,
   effect: { name: "trail", color: "aqua" },
 });
+
+// All projects display button
+
+let showMore = document.querySelector("#show-more");
+showMore.addEventListener("click", ()=>{
+  window.location.href="projects.html";
+})
+
+
+
+// Projects loading
+window.addEventListener("load", () =>{
+  loadProject();
+})
+
+let projectContainer = document.querySelector(".project-container");
+
+
+async function loadProject() {
+  try{
+    let res = await fetch("projects.json");
+    console.log(res);
+    let data = await res.json();
+    console.log(data);
+
+
+    data.forEach(project => {
+      const card = document.createElement("div");
+      
+      card.innerHTML = `<div id=${project.id} class='project fadeIn'>
+          <div class='img-frame bg-${project.image_bg}'>
+              <img src='${project.image}'>
+          </div>
+          <div class='desc'>
+            <h4>${project.title}</h4>
+            <p>${project.working_date}</p>
+            <p>${project.desc}</p>
+            <button class='button visit'>Visit website</button>
+          </div>
+      </div>`;
+      let visitBtn = card.querySelector(".visit");
+      console.log(project.link);
+      visitBtn.addEventListener("click" , () =>{
+        window.location.href = project.link;
+        
+      })
+      
+      projectContainer.append(card);
+      
+    });
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
